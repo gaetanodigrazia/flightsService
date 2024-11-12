845f677a212c09fa48d57f9fa82395cda9188ff8
gaetanodigrazia <gaetano.digrazia@bitrock.it>
2024-11-12 22:47:50 +0100

    Added SecurityConfig

--- src/main/java/com/digrazia/FlightsService/api/controller/AirportController.java
+++ src/main/java/com/digrazia/FlightsService/api/controller/AirportController.java
@@ -1,5 +1,6 @@
 package com.digrazia.FlightsService.api.controller;
 
+import com.digrazia.FlightsService.api.AirportAPI;
 import com.digrazia.FlightsService.api.FlightsAPI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-public class FlightsController implements FlightsAPI {
+@RequestMapping("/api/public")
+public class AirportController implements AirportAPI {
 
     @Override
-    @GetMapping
+    @GetMapping("/simpleGet")
     public ResponseEntity<String> simpleGet() {
         return ResponseEntity.ok("simpleGet");
     }
---
+++ src/main/java/com/digrazia/FlightsService/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.digrazia.FlightsService.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/api/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
+}
+
