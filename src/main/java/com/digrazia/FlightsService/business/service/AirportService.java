3a199b3243340510a3fba672443e5ad5da535aa8
gaetanodigrazia <gaetano.digrazia@bitrock.it>
2024-11-19 08:22:56 +0100

    Stored AirportEntity in DB

---
+++ src/main/java/com/digrazia/FlightsService/business/service/AirportService.java
@@ -0,0 +1,8 @@
+package com.digrazia.FlightsService.business.service;
+
+import com.digrazia.FlightsService.business.model.domain.AirportDomain;
+
+public interface AirportService {
+    AirportDomain airportInfo(String icao);
+
+}
