0978afb58914d25851cfb65eebf2a2fa8dc4b014
Gaetano Di Grazia <107346930+gaetanodigrazia@users.noreply.github.com>
2024-11-25 08:19:53 +0100

    Revert "Develop (#13)" (#14)
    This reverts commit 59343cc621375f32ea76109ea70b1f558399aa8a.

--- src/main/java/com/digrazia/FlightsService/config/SecurityConfig.java
+++ src/main/java/com/digrazia/FlightsService/config/SecurityConfig.java
@@ -1,35 +1,25 @@
 package com.digrazia.FlightsService.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration {
-
-    @Autowired
-    JwtAuthConverter authConverter;
-
+@EnableWebSecurity
+public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/api/user/**").hasRole("USER")
+                .authorizeRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers("/api/public/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt
-                                .jwtAuthenticationConverter(authConverter)
-                        )
-                );
-
+                .oauth2ResourceServer()
+                .jwt();
         return http.build();
     }
-
-
 }
+
