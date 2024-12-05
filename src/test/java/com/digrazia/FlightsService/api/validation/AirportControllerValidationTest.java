5774f1fb8abb32b1a48a5eacdce2839bd834d01f
gaetanodigrazia <gaetano.digrazia@bitrock.it>
2024-12-05 21:49:37 +0100

    Added check for message in test

--- src/test/java/com/digrazia/FlightsService/api/validation/AirportControllerValidationTest.java
+++ src/test/java/com/digrazia/FlightsService/api/validation/AirportControllerValidationTest.java
@@ -4,6 +4,7 @@ import com.digrazia.FlightsService.api.controller.AirportController;
 import com.digrazia.FlightsService.business.mapper.dto.AirportDTOMapper;
 import com.digrazia.FlightsService.business.mapper.dto.AirportDTOMapperTest;
 import com.digrazia.FlightsService.business.service.AirportService;
+import com.digrazia.FlightsService.business.validator.annotations.lengthControl.LengthControl;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -36,8 +40,12 @@ public class AirportControllerValidationTest {
         @Test
         @WithMockUser(username = "admin", roles = {"ADMIN"})
         void whenPathParameterTooLong_thenBadRequest() throws Exception {
-            mockMvc.perform(get("/api/admin/airportInfo/{airportIcao}", "123456789123456789123456"))
-                    .andExpect(status().isBadRequest());
+            String parameter = "123456789123456789123456";
+            MvcResult mvcResult = mockMvc.perform(get("/api/admin/airportInfo/{airportIcao}", parameter))
+                    .andExpect(status().isBadRequest())
+                    .andReturn();
+
+            assertEquals("Valore PathVariable max length: 10", mvcResult.getResponse().getContentAsString());
             }
 
 
